f3c1c8bb18055b1ac718b1f3dde2e79799f8240c
E.J. Daly <ej.daly@vetdrive.co>
2020-09-29 16:24:38 +0100

    Adding a few comments

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// Worker is a wrapper - will take whatever module you give it, and
+// create a worker thread for that module, and expose the module.exports
+// back to the main thread
+//
 const Worker = require("./worker");
 
 main();
 async function main() {
+
+  // Create a new worker from "workers/process_data.js"
+  // The result "process_data_worker_1" has the same API as the standard module.exports
+  // but runs in a separate thread
+  //
   const process_data_worker_1 = new Worker(`${__dirname }/workers/process_data.js`, {
     name: "process_data_worker_1"
   });
@@ -11,6 +20,10 @@ async function main() {
     console.log(`File: ${filename}, Result: ${result}`);
   }
   
+  // We could also just load the process_data module as normal here and
+  // use it in the main thread. The API exposed (module.exports from process_data)
+  // is exactly the same..
+  //
   const process_data_mainthread = require("./workers/process_data");
   for(let filename of ["file3", "file4"]) {
     const result = await process_data_mainthread(filename);
